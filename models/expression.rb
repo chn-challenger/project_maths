72d4a96eb9497af09ecac1ae6a965777eccd4614
Joe Zhou <chn.challenger@gmail.com>
2016-05-10 20:09:07 +0100

    modification of design - sequence diagrams and mocking

--- models/expression.rb
+++ models/expression.rb
@@ -24,6 +24,74 @@ class Expression
     end
     expanded_steps
   end
+
+################################################
+
+NumExp
+
+  def expand_add_into_ms(ms_exp)
+    return a ms_exp
+  end
+
+  def expand_sbt_into_ms(ms_exp)
+    return a ms_exp
+  end
+
+  def expand_mtp_into_ms(ms_exp)
+    return a ms_exp
+  end
+
+StringExp
+
+  def expand_add_into_ms(ms_exp)
+    return a ms_exp
+  end
+
+  def expand_sbt_into_ms(ms_exp)
+    return a ms_exp
+  end
+
+  def expand_mtp_into_ms(ms_exp)
+    return a ms_exp
+  end
+
+Expression
+
+  def expand_to_ms(ms_klass)
+    ms_expression = ms_klass.new(#empty one)
+    steps.each do |step|
+      ms_expression = step.expand_into_ms(ms_expression)
+      #no expand_ has no idea what kind of object each step is, but it doesn't matter
+      #it is a duck type that implements expand_into_ms(ms_expression)
+      #every step class is expected to implement the method.
+      #each of these implementation knows of the return object type,
+      #this type maybe taken off ms_expression.class.new
+    end
+    return ms_expression
+  end
+
+Step
+
+  def expand_into_ms
+    case :add
+      ms_expression = val.expand_add_into_ms(ms_expression)
+      #the step does not know what type of val it holds,
+      #but it knows val will implment expand_add_into_ms(ms_expression)
+      #and return another ms_expression
+      return ms_expression
+    case :sbt
+      ms_expression = val.expand_sbt_into_ms(ms_expression)
+      return ms_expression
+    case :mtp
+      ms_expression = val.expand_mtp_into_ms(ms_expression)
+      return ms_expression
+  end
+
+
+
+################################################
+
+
 end
 
 class NumExp
@@ -84,4 +152,10 @@ class StringStep
     @val = val
     @dir = dir
   end
+
+  def mtp_num_step(step)
+
+
+  end
+
 end
