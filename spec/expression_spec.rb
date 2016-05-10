2d91013f2956d9d49bf2a9ede39635e1f4a7fa59
Joe Zhou <chn.challenger@gmail.com>
2016-05-10 14:12:27 +0100

    expression copy and tests

--- spec/expression_spec.rb
+++ spec/expression_spec.rb
@@ -4,6 +4,7 @@ describe Expression do
   let(:step_1){double(:step_1)}
   let(:step_2){double(:step_2)}
   let(:exp){described_class.new([step_1,step_2])}
+  let(:exp_copy){exp.copy}
 
   describe '#new/initialize' do
     it 'initialize with an array of steps which can be read as attribute' do
@@ -26,6 +27,23 @@ describe Expression do
       step_3 = double(:step_3)
       exp_2 = described_class.new([step_3])
       expect(exp).not_to eq exp_2
+    end
+  end
+
+  describe '#copy' do
+    context 'makes a deep copy by making a copy of each step' do
+      before(:each) do
+        allow(step_1).to receive(:copy).and_return('copy_of_step_1')
+        allow(step_2).to receive(:copy).and_return('copy_of_step_2')
+      end
+
+      it 'the copy and original are different objects' do
+        expect(exp_copy.object_id).not_to eq exp.object_id
+      end
+
+      it 'steps in the copied expression are copies' do
+        expect(exp_copy.steps).to eq ['copy_of_step_1','copy_of_step_2']
+      end
     end
   end
 
