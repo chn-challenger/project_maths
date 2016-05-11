8d2a6bc95efb6e9f2e7ecbda61fdbb2d72e37ba3
Joe Zhou <chn.challenger@gmail.com>
2016-05-11 12:37:22 +0100

    clean up

--- kata.rb
+++ kata.rb
@@ -1,25 +1,17 @@
-def scan(path,map)
-  position = path.last
+def scan_possible_moves(path,map)
+  pos = path.last
   possible_moves = []
   x_max, y_max = map.length - 1, map.first.length - 1
-  if 0 <= position[0]-1 && map[position[0]-1][position[1]]
-    possible_moves << [position[0]-1,position[1]]
-  end
-  if position[1]+1 <= y_max && map[position[0]][position[1]+1]
-    possible_moves << [position[0],position[1]+1]
-  end
-  if position[0]+1 <= x_max && map[position[0]+1][position[1]]
-    possible_moves << [position[0]+1,position[1]]
-  end
-  if 0 <= position[1]-1 && map[position[0]][position[1]-1]
-    possible_moves << [position[0],position[1]-1]
-  end
-  return possible_moves - path
+  possible_moves << [pos[0]-1,pos[1]] if 0 <= pos[0]-1 && map[pos[0]-1][pos[1]]
+  possible_moves << [pos[0],pos[1]+1] if pos[1]+1 <= y_max && map[pos[0]][pos[1]+1]
+  possible_moves << [pos[0]+1,pos[1]] if pos[0]+1 <= x_max && map[pos[0]+1][pos[1]]
+  possible_moves << [pos[0],pos[1]-1] if 0 <= pos[1]-1 && map[pos[0]][pos[1]-1]
+  going_back_removed = possible_moves - path
 end
 
 def move_a_step(current_path,other_choices,map,target)
   return [] if map == [[true]]
-  possible_moves = scan(current_path,map)
+  possible_moves = scan_possible_moves(current_path,map)
   if possible_moves != []
     next_move = possible_moves.delete_at(0)
     other_choices[current_path.last] = possible_moves if possible_moves != []
@@ -27,9 +19,6 @@ def move_a_step(current_path,other_choices,map,target)
   else
     last_junction = other_choices.keys.last.dup
     junction_index = current_path.index(last_junction)
-    current_path.delete_if do |ele|
-      current_path.index(ele) > junction_index
-    end
     current_path.delete_if{|pos| current_path.index(pos) > junction_index}
     next_move = other_choices[current_path.last].delete_at(0)
     other_choices.delete(current_path.last) if other_choices[current_path.last] == []
