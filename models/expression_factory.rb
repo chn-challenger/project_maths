a1865bba8f3bc315855fde08b286db875049d404
Joe Zhou <chn.challenger@gmail.com>
2016-05-12 12:26:50 +0100

    refactored expression factory, added default step 1 ops and dir

--- models/expression_factory.rb
+++ models/expression_factory.rb
@@ -3,14 +3,29 @@ require './models/expression'
 module ExpressionFactory
   def self.create_step(step_config)
     direction = step_config[2] || :rgt
-    Step.new(step_config[0],step_config[1],direction)
+    step_class.new(step_config[0],step_config[1],direction)
   end
 
   def self.build(step_config_array)
-    steps = step_config_array.inject([]) do |result,step_config|
-      step_config[1] = build(step_config[1]) if step_config[1].is_a?(Array)
-      result << create_step(step_config)
+    steps = _build_first_step(step_config_array.first)
+    for i in 1...step_config_array.length
+      step_config_array[i][1] = build(step_config_array[i][1]) if
+        step_config_array[i][1].is_a?(Array)
+      steps << create_step(step_config_array[i])
     end
-    Expression.new(steps)
+    expression_class.new(steps)
+  end
+
+  def self._build_first_step(first_step_config)
+    first_step_config = build(first_step_config) if first_step_config.is_a?(Array)
+    [create_step([nil,first_step_config])]
+  end
+
+  def self.expression_class
+    Expression
+  end
+
+  def self.step_class
+    Step
   end
 end
--- spec/expression_factory_spec.rb
+++ spec/expression_factory_spec.rb
@@ -2,22 +2,35 @@ require './models/expression_factory'
 
 describe ExpressionFactory do
   describe '#build' do
+    it 'creates an expression of one step' do
+      step_config_array = [5]
+      expected_exp = Expression.new([Step.new(nil,5)])
+      expect(ExpressionFactory.build(step_config_array)).to eq expected_exp
+    end
+
     it 'creates an expression when given an array of step-configs' do
-      step_config_array = [[nil,5],[:mtp,'x']]
+      step_config_array = [5,[:mtp,'x']]
       expected_exp = Expression.new([Step.new(nil,5),Step.new(:mtp,'x')])
       expect(ExpressionFactory.build(step_config_array)).to eq expected_exp
     end
 
     it 'creates an expression where one of the steps has an expression value' do
-      step_config_array = [[nil,5],[:mtp,[[nil,7],[:div,'x']]]]
+      step_config_array = [5,[:mtp,[7,[:div,'x']]]]
       expected_exp = Expression.new([Step.new(nil,5),Step.new(:mtp,
         Expression.new([Step.new(nil,7),Step.new(:div,'x')]))])
       expect(ExpressionFactory.build(step_config_array)).to eq expected_exp
     end
+
     it 'creates 2 level multiple nested expression' do
-      step_config_array = [[nil,5],[:mtp,[[nil,7],[:div,[[nil,2],[:add,3,:lft]]]]]]
+      step_config_array = [5,[:mtp,[7,[:div,[2,[:add,3,:lft]]]]]]
       expected_exp = Expression.new([Step.new(nil,5),Step.new(:mtp,
         Expression.new([Step.new(nil,7),Step.new(:div,Expression.new([Step.new(nil,2),Step.new(:add,3,:lft)]))]))])
+      expect(ExpressionFactory.build(step_config_array)).to eq expected_exp
+    end
+
+    it 'creates an expression of one step whose value is an expression' do
+      step_config_array = [[2,[:add,7]]]
+      expected_exp = Expression.new([Step.new(nil,Expression.new([Step.new(nil,2),Step.new(:add,7)]))])
       expect(ExpressionFactory.build(step_config_array)).to eq expected_exp
     end
   end
