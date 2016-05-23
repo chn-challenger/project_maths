734dd88e5c44cd7f306b59cab07bb0809b43a087
Joe Zhou <chn.challenger@gmail.com>
2016-05-23 13:27:21 +0100

    bug found in collecting 2 like terms and moving

--- models/equation.rb
+++ models/equation.rb
@@ -33,7 +33,13 @@ class Equation
   def collect_like_terms
     for i in 0..left_side.steps.length-1
       for j in 0..right_side.steps.length-1
-        if (left_side.steps[i].val.is_a?(Expression) && right_side.steps[j].val.is_a?(Expression)) && left_side.steps[i].val.similar?(right_side.steps[j].val)
+        puts 'LEFT LEFT LEFT BELOW'
+        puts "curret i is #{i}"
+        p left_side.steps[i]
+        puts 'RIGHT RIGHT RIGHT BELOW'
+        puts "curret j is #{j}"
+        p right_side.steps[j]
+        if (left_side.steps[i].exp_valued? && right_side.steps[j].exp_valued?) && left_side.steps[i].val.similar?(right_side.steps[j].val)
           left_value = left_side.steps[i].val.steps.first.val
           left_value *= -1 if left_side.steps[i].ops == :sbt
           right_value = right_side.steps[j].val.steps.first.val
